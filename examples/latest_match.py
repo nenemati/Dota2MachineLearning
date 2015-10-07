92b283d43d1247e0af9915c2cde2d6439bc2fb69
Nema Nemati <nenemati@gmail.com>
2015-10-07 17:57:59 -0500

    I hate tyler

--- examples/latest_match.py
+++ examples/latest_match.py
@@ -7,6 +7,9 @@ import numpy as np
 import json
 import pymongo
 from pymongo import MongoClient 
+import json
+import numpy as np
+import pandas as pd
 
 
 client = MongoClient()
@@ -36,24 +39,78 @@ my_matches_bucket = []
 
 #my_matches_bucket.append(matches)
 
-details = api.get_match_details(match_id=1729103401)
+# details = api.get_match_details(match_id=1729103401)
+details = api.get_match_details(match_id=1729103400)
 
-db.nema.insert(details)
+# print details
+# print details
+# db.nema.insert(details)
 
 
 # print details
 # this is for 1000
 # matchesList = range(1729103401-1000,1729103401)
-matchesList = range(1729103401-10,1729103401)
+# matchesList = range(1729103401-1000,1729103401)
 
-def grab_Match_Detail(n):
+def insert_Match_Detail(n):
 	details_bucket = []
 	for p in n:
 		details = api.get_match_details(match_id=p)
 		details_bucket.append(details)
-		db.larger.insert(details)
+		db.nemaBIG.insert(details)
 		print len(details_bucket)
 
 
-grab_Match_Detail(matchesList)
+# grab_Match_Detail(matchesList)
+
+#grab all teams and heros associated with them
+
+#grab hero_id attach win or lose then attach match_number
+
+# collectionss = db.nemaBIG
+# dictionaryy = collectionss.to_mongo()
+
+
+
+def find_players_in_game():
+	# collection = db.nemaBIG
+	agg_data = db.nemaBIG.aggregate([{"$match" : {"player[0].hero_id" : "$by_user"}}]);
+		
+
+find_players_in_game()
+
+
+	# newCollection = json.load(collection)
+	# db.nemaBIG.Map_reduce(map_function, reduce_function,query='{hero_id}')
+
+	# match_number = 0
+	# team_heroes = dict()
+	# for n in matches
+	# 	count = count + 1
+
+
+	
+
+# 	# db.zipcodes.aggregate( [
+# 	#    { $hero: { _id: "$hero_id", totalPop: { $sum: "$pop" } } },
+# 	#    { $winorlose: { totalPop: { $gte: 10*1000*1000 } } }
+# 	# ])
+
+
+
+# find_players_in_game()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 		 
