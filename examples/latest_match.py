cb707183f1f2a5ba20554f345e568eb638950dca
Nema Nemati <nenemati@gmail.com>
2015-10-21 19:34:41 -0500

    cnetralized code

--- examples/latest_match.py
+++ examples/latest_match.py
@@ -10,6 +10,9 @@ from pymongo import MongoClient
 import json
 import numpy as np
 import pandas as pd
+from collections import namedtuple
+from urllib2 import Request, urlopen
+from pandas.io.json import json_normalize
 
 
 client = MongoClient()
@@ -36,7 +39,32 @@ def insert_Match_Detail(matches):
 		db.nemaBIG.insert(details)
 
 def return_filtered_matched():
-    data = db.nemaBIG.find({"$and": [{"result.game_mode": { "$nin": [0,6,7,8,9,10,11,14,15,18,19,20,21] }}, {"result.error": {"$exists": 0}} ]});
+    # data = db.nemaBIG.find({"$and": [{"result.game_mode": { "$nin": [0,6,7,8,9,10,11,14,15,18,19,20,21] }}, {"result.error": {"$exists": 0}} ]})
+    data = db.nemaBIG.find()
+
+    for d in data:
+        print d
+
+def parse_the_D(raw_data):
+    # Counter for player
+    count = 1
+    
+    # loop through players information 
+    for player_info in raw_data["results"]["players"]:
+      # print player_info
+      elevations["results"]["player_"+ count +"_hero_id"] = player_info["hero_id"]
+
+      count = count + 1
+
+    # Delete players column
+    del elevations["result"]["players"]
+    
+
+    encodedJson = json.dumps(raw_data)
+    data = json.loads(encodedJson)
+
+    # Normalize json to work with DataFrame
+    return json_normalize(data['result'])
 
 
 def main():
@@ -49,8 +77,15 @@ def main():
     matchesList = range(1729103401-10000,1729103401)
 
     # Insert match data into Mongo
-    # Comment this in to get data into your mongo database
+    # Comment the function in to get data into your mongo database
     # insert_Match_Detail(matchesList)
+    
+    # Get match data from DB into encoded JSON
     return_filtered_matched()
+    # print raw_data.count()
+    # Get raw dataframe that needs additional manipulation
+    # normalized_dataframe = parse_the_d(raw_data)
+    # print normalized_dataframe
+
 
 main()
